796befebfec8ae86f72410fa964043c2ca7cd61b
venkatesh-r <venkypsd@gmail.com>
2023-12-26 16:51:46 +0100

    header

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 import React from "react";
-import  ReactDOM from "react-dom";
-const app = React.createElement('div', {id:'root'},
-  [
-    React.createElement('div', {id: 'child1'}, 
-    React.createElement('h1', {id: 'heading'}, 'Main heading'), 
-    React.createElement('p', {class: 'para1'}, 'Paragraph tag')),
-  ],
-  [
-    React.createElement('div', {id: 'child2'}, React.createElement('h2', {id: 'heading'}, 'second heading')),
-  ]
-)
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement('h1', {id: 'heading'}, 'Hello React JS');
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(app);
-console.log(app)
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxPIeEEhgJRQsNLtDeCKPgDoiT4Q0x_7k4w1UWLNaWcOn89smKWTHOHRMP03BCS-74hE8&usqp=CAU"></img>
+            </div>
+            <div className="nav-bar">
+                <ul>
+                  <li>Home</li> 
+                  <li>About</li>  
+                  <li>Contact</li>  
+                  <li>cart</li>     
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+const AppLayout = () => {
+    return (
+        <Header/>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+root.render(<AppLayout/>);
