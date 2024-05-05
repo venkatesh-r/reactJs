457dc86c0354f6ff4ca6c2cc75b6c2fa8ab2f09a
venkatesh-r <venkypsd@gmail.com>
2024-05-05 17:50:43 +0200

    user data added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,45 @@
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import { LOGO_URL } from '../utils/constants';
-
+import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { LOGO_URL } from "../utils/constants";
+import userContext from "../utils/userContext";
 
 const Header = () => {
-const [loginBtn, setLoginBtn] = useState("Login");
-    return (
-        <div className="flex justify-between bg-gray-100">
-            <div className="w-24">
-                <img src={LOGO_URL}></img>
-            </div>
-            <div className="flex items-center">
-                <ul className='flex p-4 m-4'>
-                  <li className='px-4'>
-                    <Link to="/">Home</Link>
-                  </li> 
-                  <li className='px-4'>
-                    <Link to="/about">About</Link>
-                  </li>  
-                  <li className='px-4'>
-                    <Link to="/grocery">Grocery</Link>
-                  </li> 
-                  <li className='px-4'>
-                    <Link to="/contact">Contact Us</Link>
-                  </li>  
-                  <li className='px-4'>cart</li> 
-                  <button
-                  onClick={()=>{
-                    loginBtn === 'Login'? setLoginBtn("Logout"):setLoginBtn("Login");
-                  }}
-                  >
-                    {loginBtn}
-                  </button>    
-                </ul>
-            </div>
-        </div>
-    );
-}
+  const [loginBtn, setLoginBtn] = useState("Login");
+  const { loggedInUser } = useContext(userContext);
+  return (
+    <div className="flex justify-between bg-gray-100">
+      <div className="w-24">
+        <img src={LOGO_URL}></img>
+      </div>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="px-4">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/grocery">Grocery</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li className="px-4">cart</li>
+          <button
+            onClick={() => {
+              loginBtn === "Login"
+                ? setLoginBtn("Logout")
+                : setLoginBtn("Login");
+            }}
+          >
+            {loginBtn}
+          </button>
+          <li className="px-4">{loggedInUser}</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
