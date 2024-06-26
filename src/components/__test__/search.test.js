537dc4552bd47c51cc43f00fa7285bc756d54945
venkatesh-r <venkypsd@gmail.com>
2024-06-26 19:57:19 +0200

    mock data for search test

--- src/components/__test__/search.test.js
+++ src/components/__test__/search.test.js
@@ -1,6 +1,23 @@
 import { render } from "@testing-library/react";
 import Body from "../Body";
+import { BrowserRouter, json } from "react-router-dom";
+import Mock_Data from "../mocks/mockresListdata";
+import { act } from "react";
 
-it("Search test button", () => {
-  render(<Body />);
+global.fetch = jest.fn(() => {
+  return Promise.resolve({
+    json: () => {
+      return Promise.resolve(Mock_Data);
+    },
+  });
+});
+
+it("Search test button", async () => {
+  await act(async () =>
+    render(
+      <BrowserRouter>
+        <Body />
+      </BrowserRouter>
+    )
+  );
 });
